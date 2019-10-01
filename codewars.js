881deb7e7fbe82568c8f843d6713cc96a9e7ef43
andregcab <cabrerandre@gmail.com>
2019-10-01 11:49:48 -0400

    completed challenge

--- codewars.js
+++ codewars.js
@@ -3,3 +3,24 @@
 //messi goals
 
 const goals = (ll, cdr, cl) => ll + cdr + cl
+
+//Cat years, dog years(2) > Could have done better...
+
+var ownedCatAndDog = function(catYears, dogYears) {
+    let ownedYears = []
+    const earlyYears = (years) => years >= 24 ? 2 : years >= 15 ? 1 : 0;
+  
+    if(catYears >= 28) {
+      ownedYears.push(Math.floor((catYears - 24) / 4)+2)
+     } else {
+      ownedYears.push(earlyYears(catYears))
+    }
+  
+    if (dogYears >= 29) {
+      ownedYears.push(Math.floor((dogYears - 24) / 5)+2)
+    } else {
+      ownedYears.push(earlyYears(dogYears))
+    }
+    
+    return ownedYears;
+  }
